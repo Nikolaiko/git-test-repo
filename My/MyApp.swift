aa2a8acece8f9d2186e0c10896ef5595fdca29f2
Nikolai Baklanov <nikolaiko@mail.ru>
2023-05-18 23:57:50 +0600

    Next commit

---
+++ My/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  My
+//
+//  Created by Nikolai Baklanov on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
